cb27f89b0e2af5f3e529bf8ae4af88efdf70c685
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-09-05 15:24:33 +0200

    Transfer transaction tests

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -14,15 +14,22 @@ const (
 	dbSource = "postgresql://postgres:mysecretpassword@localhost:5432/go4money?sslmode=disable"
 )
 
+// Global variables
 var testQueries *Queries
+var testDB *sql.DB
 
 func TestMain(m *testing.M) {
-	conn, err := sql.Open(dbDriver, dbSource)
+	var err error
+
+	// Database connections
+	testDB, err = sql.Open(dbDriver, dbSource)
 	if err != nil {
 		log.Fatal("Error while connecting to db:", err)
 	}
 
-	testQueries = New(conn)
+	// Queries and functions allowed
+	testQueries = New(testDB)
 
+	// Execute Go tests and then finish
 	os.Exit(m.Run())
 }
--- db/sqlc/store.go
+++ db/sqlc/store.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 )
 
+// Provides all functions to execute DB queries
 type Store struct {
 	*Queries
 	db *sql.DB
@@ -13,12 +14,17 @@ type Store struct {
 
 func NewStore(db *sql.DB) *Store {
 	return &Store{
-		db:      db,
 		Queries: New(db),
+		db:      db,
 	}
 }
 
 // Executes a function within a database transaction
+// @Params
+// @Store -> DB Connection and the queries allowed
+// @Context
+// @Function -> DB function to execute
+// @Returns error. Can be nil or contains an error
 func (store *Store) execTx(ctx context.Context, fn func(*Queries) error) error {
 	tx, err := store.db.BeginTx(ctx, nil)
 	if err != nil {
@@ -51,16 +57,42 @@ type TransferTxResult struct {
 	ToEntry     Entry    `json:"to_entry"`
 }
 
+// Performs a money transfer between two accounts.
+// It will create a transfer record, add account entries and update accounts balance
+// @Params
+// @Store -> DB Connection and the queries allowed
+// @Context
+// @TransferTxParams -> Parameters of a transaction
+// @Returns TransferTxResult and error
 func (store *Store) TransferTx(ctx context.Context, arg TransferTxParams) (TransferTxResult, error) {
 	var result TransferTxResult
 
 	err := store.execTx(ctx, func(q *Queries) error {
 		var err error
 
+		// Call the DB function to create transfer transfer.sql.go
 		result.Transfer, err = q.CreateTransfer(ctx, CreateTransferParams{
 			FromAccountID: arg.FromAccountID,
 			ToAccountID:   arg.ToAccountID,
 			Amount:        arg.Amount,
+		})
+		if err != nil {
+			return err
+		}
+
+		// Call the DB function to create entry entry.sql.go
+		result.FromEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.FromAccountID,
+			Amount:    -arg.Amount, // Negative amount as his amount decreases with transfers
+		})
+		if err != nil {
+			return err
+		}
+
+		// Call the DB function to create entry entry.sql.go
+		result.ToEntry, err = q.CreateEntry(ctx, CreateEntryParams{
+			AccountID: arg.ToAccountID,
+			Amount:    arg.Amount,
 		})
 		if err != nil {
 			return err
