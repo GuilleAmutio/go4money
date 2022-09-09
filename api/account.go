70b6eb6ca9925ae03c99f3bcd9f7df0446a3196f
Guillermo Amutio Duarte <guillermo.amutio-duarte@capgemini.com>
2022-09-09 12:31:12 +0200

    Control errors

--- api/account.go
+++ api/account.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	db "github.com/guilleamutio/go4money/db/sqlc"
+	"github.com/lib/pq"
 )
 
 // POST Requests
@@ -29,6 +30,13 @@ func (server *Server) createAccount(ctx *gin.Context) {
 
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "foreign_key_violation", "unique_violation":
+				ctx.JSON(http.StatusForbidden, errorResponse(err))
+				return
+			}
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
